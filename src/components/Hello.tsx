5a62d2b56623a83210670a47d5649665e03db6b9
petelina17 <petelina17@gmail.com>
2020-02-16 15:27:21 +0100

    npm script start

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,8 +3,6 @@ import './Hello.css'
 
 export interface HelloProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
         return <div className={'hello'}>
